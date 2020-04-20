5b82df17c99be93f8877098e2284b0be95f887d9
Scott Underwood <s-underwood@wiu.edu>
2020-04-19 19:27:06 -0500

    sfx added to tracks DONE

--- src/Collections/Tracks.java
+++ src/Collections/Tracks.java
@@ -12,7 +12,13 @@ package Collections;
 public enum Tracks {
        
     TRACK_ZERO(0, "Start Theme", "/music/Ev23.wav"),
-    TRACK_ONE(1,"Redo Start Theme","music/workYet.wav");
+    TRACK_ONE(1,"Redo Start Theme","/music/workYet.wav"),
+    TRACK_TWO(2,"Death Grunt","/soundEffects/deathGrunt.wav"),
+    TRAC_THREE(3,"Electricution","/soundEffects/electricution.mp3"),
+    TRACK_FOUR(4,"Footsteps on pavement","/soundEffects/footStepsPavement.wav"),
+    TRACK_FIVE(5,"Footsteps on metal","/soundEffects/footstepsMetal.wav"),
+    TRACK_SIX(6,"Lighter Footsteps","/soundEffects/lighterFootsteps.mp3"),
+    TRACK_SEVEN(7,"Victory!!!","/soundEffects/victorySound.wav");
     
     private final int number;
     private final String description;
